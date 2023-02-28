f5210fc5ecc2bdad2b62258be76298c3efc35a3f
Kyle Granger <kyle.granger@chello.at>
2023-02-28 14:34:01 +0100

    feat: add localhost SSL certificates, support https server (#17)

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,16 @@
 import express from "express"
 import path from "path"
 import http from "http"
+import https from "https"
+import fs from "fs"
 
 const port: number = 3000
+let useSsl: boolean = true;
+
+if (process.argv.length > 2 && process.argv[2] == 'http') {
+    console.log('Do not use secure SSL server');
+    useSsl = false;
+}
 
 class App {
     private server: http.Server
@@ -12,7 +20,16 @@ class App {
         this.port = port;
         const app = express();
         app.use(express.static(path.join(__dirname, '../client')));
-        this.server = new http.Server(app);
+
+        if (useSsl) {
+            const options = {
+                key: fs.readFileSync('./config/key.pem'),
+                cert: fs.readFileSync('./config/cert.pem')
+            };
+            this.server = https.createServer(options, app);
+        } else {
+            this.server = new http.Server(app);
+        }
     }
 
     public Start() {
